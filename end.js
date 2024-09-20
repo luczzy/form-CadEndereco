0a368022ac01465c152452b88cca8dae09b3e192
Deivid Lucas <deivid.marques@alunoescolassociais.org.br>
2024-09-20 11:01:54 -0300

    Inicia projeto de validação de endereço

---
+++ end.js
@@ -0,0 +1,3 @@
+'use strict'; // ativa o modo restrito
+// Este modo faz com o javascript opera de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação.
+/* consumo de API - https://viacep.com.br/ */
