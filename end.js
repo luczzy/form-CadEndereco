bcf51997421ec022ce5cd968eaca828eae6279b3
Deivid Lucas <deivid.marques@alunoescolassociais.org.br>
2024-09-20 11:15:01 -0300

    Adiciona const limparformulario

--- end.js
+++ end.js
@@ -1,3 +1,15 @@
 'use strict'; // ativa o modo restrito
 // Este modo faz com o javascript opera de forma mais segura e rigorosa, ajudando a evitar erros comuns de programação.
 /* consumo de API - https://viacep.com.br/ */
+
+
+// função para limpar campos preenchidos
+
+const limparFormulario = () => {
+    document.getElementById ('cep').value = '';
+    document.getElementById ('logradouro').value = '';
+    document.getElementById ('bairro').value = '';
+    document.getElementById ('complemento').value = '';
+    document.getElementById ('localidade').value = '';
+    document.getElementById ('uf').value = '';
+}
